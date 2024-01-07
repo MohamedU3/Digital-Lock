c0dafb5e7ed59b9a3432e767eba0084482d9ac6d
Mohamed Yousry <mohamed.yousry179@gmail.com>
2024-01-07 22:54:54 +0200

    Update config.h

--- config.h
+++ config.h
@@ -87,7 +87,7 @@ extern "C" {
 #pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
 #pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
 #pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config CPD = ON         // Data EEPROM Memory Code Protection bit (Data EEPROM code-protected)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
@@ -95,5 +95,8 @@ extern "C" {
 // Use project enums instead of #define for ON and OFF.
 
 #include <xc.h>
+
+
+
 
 
